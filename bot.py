4cb685251c1f349ded5ded2247f9de82b51c4e2d
Kendall Chien <kendallchien@gmail.com>
2022-01-22 02:11:32 -0800

    fixes to ratsignal, add coward counter

--- bot.py
+++ bot.py
@@ -3,14 +3,13 @@ import os
 import asyncio
 import random
 import yaml
+import time
 from dotenv import load_dotenv
 from discord.ext import commands
 
 with open('img.yaml') as f:
-    
-    data = yaml.load(f, Loader=yaml.FullLoader)
-    print(data)
 
+    data = yaml.load(f, Loader=yaml.FullLoader)
 
 # LOAD .ENV FILE
 load_dotenv()
@@ -95,94 +94,88 @@ async def on_raw_reaction_remove(payload):
         else:
             print("Role not found")
 
-# @bot.event
-# async def on_message(message):
-#     if message.author == bot.user:
-#         return
-
-#     if message.content.startswith('!ratbastards'):
-#         msg = await message.channel.send("""konichiwha @ratbastards!
-#             @{0.author} cals for aid!
-#             """.format(message))
-#         print(msg)
-
-#     if message.content == '!ratbastardsignal':
-#         await message.add_reaction('\U0001F987')
-
-#     await bot.process_commands(message)
-
 # rat signal 
 @bot.command(pass_context=True)
 async def ratsignal(ctx):
 
+    quote = random.choice(data.get('emiya'))
+
+    win_probability = random.randint(0, 100)
+
+    coward_users = ''
+    cnt_ready = 0
+    users = ''
+
+    if win_probability <= 10:
+        win_url = data.get('img').get('img1').get('link')
+
+    elif win_probability <= 20:
+        win_url = data.get('img').get('img2').get('link')
+
+    elif win_probability <= 30:
+        win_url = data.get('img').get('img3').get('link')                        
+
+    elif win_probability <= 40:
+        win_url = data.get('img').get('img4').get('link')
+
+    elif win_probability <= 50:
+        win_url = data.get('img').get('img5').get('link')                                                
+
+    elif win_probability <= 60:
+        win_url = data.get('img').get('img6').get('link')
+
+    elif win_probability <= 70:
+        win_url = data.get('img').get('img7').get('link')
+
+    elif win_probability <= 80:
+        win_url = data.get('img').get('img8').get('link')                        
+
+    elif win_probability <= 90:
+        win_url = data.get('img').get('img9').get('link')
+
+    elif win_probability <= 100:
+        win_url = data.get('img').get('img10').get('link')
+
+
     emb_msg = '''
-        konichiwha you @ratbastards! {0} calls for aid!
+        konichiwha you <@934357238217326632>! {0} calls for aid!
     '''.format(ctx.author.mention)
+
+    
     
     emb = discord.Embed(title='RATSIGNAL', description=emb_msg, color=16769251)
-    # emb.set_thumbnail(url='https://www.anime-planet.com/images/characters/3799.jpg?t=1615919953')
-    emb.set_image(url='https://www.anime-planet.com/images/characters/3799.jpg?t=1615919953')
-
-    emb.add_field(name='Ready', value='---', inline=True)
-    emb.add_field(name='Not Ready: ', value='---', inline=True)
-    emb.add_field(name='Rat Bastards: ', value='HERE ALL USERS WITH ✅', inline=False)
-
-
+    emb.set_image(url='https://media.giphy.com/media/2y98KScHKeaQM/giphy.gif')
+    emb.add_field(name='READY', value='---', inline=False)
+    emb.add_field(name='Rat Bastards: ', value='HERE ALL USERS WITH ✅', inline=True)
+    emb.add_field(name='COWARDS: ', value='---', inline=True)
+    emb.set_footer(text=quote)
+    
     msg = await ctx.channel.send(embed=emb)    
-    # msg = await ctx.channel.send("""konichiwha @ratbastards!
-    #     {0} cals for aid!
-    #     """.format(ctx.author.mention))
+
     await msg.add_reaction('✅') # check     
     await msg.add_reaction('❌') # x
     await msg.add_reaction('\U0001F987') # bat
 
-
     # create a while loop that will wait for users to react
-
-    # not_ready = 0
-    # cnt_ready = 0    
 
     while True:
         
         users = "" 
-        win_probability = random.randint(0, 100)
+        coward_users = ""
 
-        if win_probability <= 10:
-            win_url = data.get('img').get('img1').get('link')
 
-        elif win_probability <= 20:
-            win_url = data.get('img').get('img2').get('link')
+        emojis = ['✅', '❌']
 
-        elif win_probability <= 30:
-            win_url = data.get('img').get('img3').get('link')                        
 
-        elif win_probability <= 40:
-            win_url = data.get('img').get('img4').get('link')
-
-        elif win_probability <= 50:
-            win_url = data.get('img').get('img5').get('link')                                                
-
-        elif win_probability <= 60:
-            win_url = data.get('img').get('img6').get('link')
-
-        elif win_probability <= 70:
-            win_url = data.get('img').get('img7').get('link')
-
-        elif win_probability <= 80:
-            win_url = data.get('img').get('img8').get('link')                        
-
-        elif win_probability <= 90:
-            win_url = data.get('img').get('img9').get('link')
-
-        elif win_probability <= 100:
-            win_url = data.get('img').get('img10').get('link')
+        def check(reaction, user):
+            return (reaction.message.id == msg.id) and (user.bot != True) and (str(reaction.emoji) in emojis)
 
 
         tasks = [
             asyncio.create_task(
                 bot.wait_for(
                     'reaction_add',
-                     check=lambda reaction, user: reaction.emoji == "✅",
+                     check = check,
                      timeout=600
                  ), 
                 name='radd'
@@ -190,8 +183,8 @@ async def ratsignal(ctx):
             asyncio.create_task(
                 bot.wait_for(
                     'raw_reaction_remove',
-                     timeout=600,
-                     check=lambda payload: str(payload.emoji) == "✅" and payload.message_id == msg.id
+                    check=lambda payload: str(payload.emoji) in emojis and payload.message_id == msg.id,
+                     timeout=600                     
                  ), 
                 name='rrem'
             )            
@@ -211,9 +204,10 @@ async def ratsignal(ctx):
                 reaction, user = result
                 
                 for i, reactions in enumerate(msg.reactions):
-                    print("----add loop '{0}', {1}".format(i, reactions))
 
                     if str(reactions) == "✅":
+
+                        win_probability =  min(max(win_probability+ random.randint(-10,10), 0), 100)
 
                         user_list = [user async for user in reactions.users() if user != bot.user]
 
@@ -222,15 +216,23 @@ async def ratsignal(ctx):
 
                         cnt_ready = len(user_list)
 
+                    elif str(reactions) == "❌":
+
+                        user_list = [user async for user in reactions.users() if user != bot.user]
+
+                        for user in set(user_list):
+                            coward_users = coward_users + user.mention + "\n"
+
+                        not_ready = len(user_list)
+
                 # create updated embed
                 emb1 = discord.Embed(title='RATSIGNAL', description=emb_msg, color=16769251)
-                # emb.set_thumbnail(url='https://www.anime-planet.com/images/characters/3799.jpg?t=1615919953')
                 emb1.set_image(url=win_url)
-                emb1.add_field(name='Ready', value='({0}/5)'.format(str(cnt_ready)), inline=True)
-                emb1.add_field(name='Not Ready: ', value='---', inline=True)
-                # emb1.add_field(name='Not Ready', value='({0}/5)'.format(str(not_ready)), inline=True)
-                emb1.add_field(name='Rat Bastards: ', value='HERE ALL USERS WITH ✅ \n' + users, inline=False)
+                emb1.add_field(name='READY', value='({0}/5)'.format(str(cnt_ready)), inline=False)
+                emb1.add_field(name='RAT BASTARDS: ', value='HERE ALL USERS WITH ✅ \n' + users, inline=True)
+                emb1.add_field(name='COWARDS: ', value='--- \n' + coward_users, inline=True)                
                 emb1.add_field(name='Win Probability: ', value='{0}%'.format(win_probability), inline=False)
+                emb1.set_footer(text=quote)
 
                 await msg.edit(embed = emb1)      
 
@@ -238,7 +240,6 @@ async def ratsignal(ctx):
             elif action == 'rrem':
 
                 for i, reactions in enumerate(msg.reactions):
-                    print("-----remove loop '{0}', {1}".format(i, reactions))
 
                     if str(reactions) == "✅":
                         user_list = [user async for user in reactions.users() if user != bot.user]
@@ -248,14 +249,23 @@ async def ratsignal(ctx):
 
                         cnt_ready = len(user_list)
 
+                    elif str(reactions) == "❌":
+
+                        user_list = [user async for user in reactions.users() if user != bot.user]
+
+                        for user in set(user_list):
+                            coward_users = coward_users + user.mention + "\n"
+
+                        not_ready = len(user_list)    
+
+                # create updated embed            
                 emb1 = discord.Embed(title='RATSIGNAL', description=emb_msg, color=16769251)
-                # emb.set_thumbnail(url='https://www.anime-planet.com/images/characters/3799.jpg?t=1615919953')
                 emb1.set_image(url=win_url)
-                emb1.add_field(name='Ready', value='({0}/5)'.format(str(cnt_ready)), inline=True)
-                emb1.add_field(name='Not Ready: ', value='---', inline=True)
-                # emb1.add_field(name='Not Ready', value='({0}/5)'.format(str(not_ready)), inline=True)
-                emb1.add_field(name='Rat Bastards: ', value='HERE ALL USERS WITH ✅ \n' + users, inline=False)
-                emb1.add_field(name='Win Probability: ', value='{0}%'.format(win_probability), inline=False)
+                emb1.add_field(name='READY', value='({0}/5)'.format(str(cnt_ready)), inline=False)
+                emb1.add_field(name='RAT BASTARDS: ', value='HERE ALL USERS WITH ✅ \n' + users, inline=True)
+                emb1.add_field(name='COWARDS: ', value='--- \n' + coward_users, inline=True)                
+                emb1.add_field(name='Win Probability: ', value='{0}%'.format(win_probability), inline=False) 
+                emb1.set_footer(text=quote)
 
                 await msg.edit(embed = emb1)
 
