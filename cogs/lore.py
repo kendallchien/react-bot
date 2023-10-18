b4cea6f1f1f5ae6d7b43b38687a5bbc0f7de7aa3
Kendall Chien <kendallchien@gmail.com>
2023-10-18 13:02:08 -0700

    fix some prompts

--- cogs/lore.py
+++ cogs/lore.py
@@ -4,6 +4,7 @@ from discord.ext import commands
 import json
 import openai
 from datetime import datetime 
+import asyncio
 import os 
 
 
@@ -14,21 +15,7 @@ class Lore(commands.Cog):
         os.makedirs(os.path.dirname('data/'), exist_ok=True)
         self.prompts = {
             '83311868432617472': """
-                I will send you some information collected from a Discord bot I wrote. This Discord bot has a lore command that allows users to submit information, which is being aggregated here. The JSON file contains user-submitted entries, each formatted as a JSON string that includes the user who submitted the entry and the timestamp.
-
-                I want you to turn the information from these JSON-formatted entries into a short story, just a few paragraphs, written in a fantasy style.
-
-                Here's some more information for you:
-                - In this fantasy world, the land should be called "Yahallo."
-                - Brian is the hottest dude at the peak
-                - Stephanie has a juicy booty
-                - Kendall is a self pro-claimed hero of justice 
-                - Kelly is secretively a vampire
-                - Andy has a expansive knife collection
-                
-                Please keep the response as short as possible but still comprehensive. You do not need to use ALL the information given for the story. I would prioritize the most recent entries when building a story, but also mix in some older entries occasionally. 
-
-                Please use this background to craft a fantastical tale that transports readers to the enchanting realm of "Yahallo." Incorporate the lore and stories shared by the bot's users from the JSON file into the narrative, infusing the story with elements of magic, adventure, and wonder. Let your creativity flow as you paint a vivid picture of this mystical world and its captivating tales. Keep the response within 500 characters
+                Discord server name: Yahallo
                 """,
             '416439957444362253': """
                 I will send you some information collected from a Discord bot I wrote. This Discord bot has a lore command that allows users to submit information, which is being aggregated here. The JSON file contains user-submitted entries, each formatted as a JSON string that includes the user who submitted the entry and the timestamp.
@@ -70,6 +57,7 @@ class Lore(commands.Cog):
 
     async def lore_summary(self, interaction:discord.Interaction, prompt: str=""):
         try:        
+            await interaction.response.defer()
             self.guild_id = interaction.guild_id 
             self.data_file = f'data/{interaction.guild.id}_lore_data.json'
 
@@ -78,10 +66,15 @@ class Lore(commands.Cog):
             lore_entries = await self.load_lore_data()
 
             # Generate a summary (you can replace this with your AI-based summary generation)
-            summary = await self.ll(lore_entries, prompt)
 
-            await interaction.response.send_message(f">>> {summary}")
-        except:
+            summary = await self.generate_fantasy_story(lore_entries, prompt)
+            await asyncio.sleep(delay=3)
+
+            # await interaction.response.send_message()
+            
+            await interaction.followup.send(f">>> {summary}")            
+            # await interaction.response.send_message(summary)
+        except Exception as e:
             print(f"An error occured {e}")
 
     async def load_lore_data(self):
@@ -102,20 +95,30 @@ class Lore(commands.Cog):
         try:
             openai.api_key = self.token
 
-            # Construct the base prompt with your desired fantasy-style instructions
-            base_prompt = self.prompts.get(str(self.guild_id), "Use information provided that is JSON converted to string to tell a short story in less than 500 characters. Keep things concise")
+            # Base Prompt
+            base_prompt = '''
+                You are a Discord server bot that shares snippets of lore based on user-submitted entries.
+                Your role is to craft these pieces of information into a captivating short story written in a fantasy style.
+                Please keep the story under 800 characters and focus more on recent entries.
+            '''
 
-            # Check if additional_context is provided, and if so, emphasize its importance in the prompt
+            # Server-specific content
+            
+            guild_context = self.prompts.get(str(self.guild_id), "")
+
+            # Combine base, server-specific, and additional context
+            prompt = base_prompt
+            if guild_context:
+                prompt += f'\nServer Specific Context:" {guild_context}'
+            
             if additional_context:
-                prompt = f"IMPORTANT: {additional_context}\n\n{base_prompt}"
-            else:
-                prompt = base_prompt
+                prompt += f'\High Priority Context:" {additional_context}'
 
             # Convert lore entries to JSON string
             lore_entries_json = json.dumps(lore_entries)
 
-            # Concatenate lore entries and prompt
-            lore_text =  prompt + "\n\n" + lore_entries_json
+            # Assemble final prompt
+            lore_text = prompt + "\n\nUser-Submitted Entries:\n" + lore_entries_json
 
             # Generate the story using GPT-3
             response = openai.Completion.create(
